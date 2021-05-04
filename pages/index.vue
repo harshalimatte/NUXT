94504d2309efa67a708e7ab4a677b86582693f96
Harshali Matte <harshalimatte10@gmail.com>
2021-05-04 10:57:39 +0100

    axios small changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,37 @@
 <template>
-  <div><h1>Events</h1></div>
+  <div>
+    <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
+  </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
   head() {
     return {
       title: 'Event Listing',
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch((e) => {})
+  },
+  components: {
+    EventCard,
   },
 }
 </script>
